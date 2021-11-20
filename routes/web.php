12783a992bff4e494f847591d2c804f72c92fd73
mf-rozi <m.fakhrulrozi10@gmail.com>
2021-11-21 00:29:06 +0700

    layouting

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('backend', function () {
+    return view('backend.dashboard');
+});
+Route::get('bekend', function () {
+    return view('backend.dashbor',[
+        'title' => 'Dashboard',
+    ]);
+});
