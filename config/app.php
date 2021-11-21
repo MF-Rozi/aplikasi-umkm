16918d421e3dfd25dcc718094b1e53f3827c0b4c
Adryan Eka Vandra <adryanekavandra@gmail.com>
2021-11-21 19:03:49 +0700

    add laravel permission

--- config/app.php
+++ config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * External Service Providers...
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
+
+
     ],
 
     /*
