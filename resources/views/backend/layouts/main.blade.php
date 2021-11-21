5dcb9405e7c89e96c3ab7fd34a2719cfd0f068bf
Adryan Eka Vandra <adryanekavandra@gmail.com>
2021-11-21 18:42:22 +0700

    change to mix generated file

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -12,15 +12,15 @@
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="backend/assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="backend/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="backend/assets/css/nucleo.css" rel="stylesheet" />
+
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="backend/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="backend/assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+  <link id="pagestyle" href="backend/assets/css/backend.css" rel="stylesheet" />
 </head>
 <body>
     @include('backend.partials.sidebar')
 
+    <script src="backend/assets/js/backend.js"></script>
 </body>
