2dc44ef8e0fbe0117f9a7904f32d3a45a36b71ab
mf-rozi <m.fakhrulrozi10@gmail.com>
2021-12-18 17:22:33 +0700

    edit photo path

--- resources/views/backend/user/show.blade.php
+++ resources/views/backend/user/show.blade.php
@@ -9,7 +9,7 @@
         <div class="row gx-4">
             <div class="col-auto">
                 <div class="avatar avatar-xl position-relative">
-                    <img src="{{ asset("storage/users/image/{$userDetail->profile->photo_profile}"); }}" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+                    <img src="{{ asset("storage/image/users/{$userDetail->profile->photo_profile}"); }}" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
                 </div>
             </div>
             <div class="col-auto my-auto">
