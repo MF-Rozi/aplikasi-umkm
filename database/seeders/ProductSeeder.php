e018f788ee9e2c438c64063399abd7b0153ad41e
MF-Rozi <m.fakhrulrozi10@gmail.com>
2021-12-19 09:43:45 +0700

    add productSeeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'name'=>"Kopi",
+            'price'=>5000,
+            'stock'=>100,
+            'code'=>'D-K-1',
+        ]);
+    }
+}
