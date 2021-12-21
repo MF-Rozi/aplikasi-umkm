259ae177c3467190648abb0206e88050746adb47
Adryan Eka Vandra <adryanekavandra@gmail.com>
2021-12-21 23:56:27 +0700

    payment

---
+++ database/migrations/2021_12_21_143412_add_payload_to_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPayloadToPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+          $table->json('payload')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn('payload');
+
+        });
+    }
+}
