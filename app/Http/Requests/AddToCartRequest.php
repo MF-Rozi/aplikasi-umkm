385597a1f90fd40c000cb3a9f01603effd2ce1d6
MF-Rozi <m.fakhrulrozi10@gmail.com>
2021-12-22 20:39:38 +0700

    add cart page

---
+++ app/Http/Requests/AddToCartRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddToCartRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required',
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'quantity' => 'required|numeric',
+            'attributes' => 'nullable',
+        ];
+    }
+}
--- resources/views/frontend/layouts/partials/navbar.blade.php
+++ resources/views/frontend/layouts/partials/navbar.blade.php
@@ -62,7 +62,7 @@
                                     @else
                                     <a href="{{ route('login') }}">Login</a>
                                     @endif
-                                    <a class="shopping-cart" href="cart.html"><i class="fas fa-shopping-cart"></i></a>
+                                    <a class="shopping-cart" href="{{ route('frontend.cart.index') }}"><i class="fas fa-shopping-cart"></i></a>
                                     {{-- <a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a> --}}
 
 
